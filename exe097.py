aac99e72af0f50f5cbf2fd4aa2c81ec6e40f2a4b
Guilherme Felisola <guifelisola@hotmail.com>
2024-07-08 12:33:52 -0300

    estudos em def

---
+++ exe097.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+#Programa Principal
+escreva('Guilherme Felisola')
+escreva('Curso de Python')
+escreva('CeV')
